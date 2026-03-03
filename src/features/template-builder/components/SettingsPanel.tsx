67e004eb1c43527577fc7a9a1bcd9cfe324e344f
charlzpaul <charlzpaul@gmail.com>
2026-03-02 19:28:57 -0500

    fixx

--- src/features/template-builder/components/SettingsPanel.tsx
+++ src/features/template-builder/components/SettingsPanel.tsx
@@ -1,6 +1,5 @@
 // src/features/template-builder/components/SettingsPanel.tsx
 import { useMemo, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useTemplateStore } from '../store/useTemplateStore';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
