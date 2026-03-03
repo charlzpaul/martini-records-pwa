977bebf9b5df61b5753984faa45ef5a47b99cd86
charlzpaul <charlzpaul@gmail.com>
2026-03-02 22:09:43 -0500

    fix for rename martini-records-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 	'/invoicePWApp/',
+  base: 	'/martini-records-pwa/',
   plugins: [
     react(),
     nodePolyfills({
